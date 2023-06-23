b7ebb4df7933c9f5d05707030e1b472ee9b4cbfa
flyphoenix31 <flyphoenix31@gmail.com>
2023-06-23 12:42:08 -0700

    ?

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const si = require('systeminformation');
 const { app, BrowserWindow, ipcMain } = require('electron');
 const express = require('express');
 const http = require('http');
@@ -5,6 +6,9 @@ const express_app = express();
 const httpServer = http.createServer(express_app);
 const io = require('socket.io')(httpServer);
 const path = require('path');
+const child_process = require('child_process');
+
+const mysql_helper = require('./mysql_helper');
 
 const router = require('./web/routes');
 const ioHandler = require('./web/ioHandler');
@@ -33,28 +37,18 @@ app.whenReady().then(() => {
 
 app.on('window-all-closed', async () => {
     if (process.platform !== 'darwin') {
-        new Promise(resolve => {
-            child_process.exec(`\"${__dirname}\\mysql\\server\\bin\\mysqladmin.exe\" -u root -p --password=\"123456789\" shutdown`, () => {
-                resolve();
-            })
-        });
+        // await new Promise(resolve => {
+        //     child_process.exec(`\"${__dirname}\\mysql\\server\\bin\\mysqladmin.exe\" -u root -p --password=\"123456789\" shutdown`, () => {
+        //         resolve();
+        //     })
+        // });
+        await mysql_helper.killServer();
+        console.log('quiting...');
         app.quit();
     }
 })
 
-var child_process = require('child_process');
-var mysql_process = child_process.exec(`\"${__dirname}\\mysql\\server\\bin\\mysqld.exe\" --defaults-file=\"${__dirname}\\mysql\\data\\my.ini\"`, (error, stdout, stderr) => {
-    console.log("error:", error);
-    console.log("stdout:", stdout);
-    console.log("stderr:", stderr);
-});
-
-// mysql_process.on("spawn", () => console.log("spawn"));
-// mysql_process.on("close", (number, signal) => console.log("close:", number, signal));
-// mysql_process.on("disconnect", () => console.log("disconnect"));
-// mysql_process.on("error", (err) => console.log("error:", err));
-// mysql_process.on("exit", (number, signal) => console.log("exit:", number, signal));
-// mysql_process.on("message", (message) => console.log("message:", message));
+mysql_helper.startServer();
 
 express_app.use(express.static('web/client'));
 express_app.use('/', router);
@@ -64,3 +58,9 @@ io.on('connection', ioHandler);
 httpServer.listen(9000, () => {
     console.log('server is listening on port 9000');
 })
+
+// si.processes().then(data => {
+//     // console.log(Object.keys(data));
+//     // console.log(data.list[0]);
+// })
+// si.networkStats("pmarket_mysqld.exe").then(data => console.log("data:", data));
---
+++ mysql_helper.js
@@ -0,0 +1,133 @@
+const si = require('systeminformation');
+var child_process = require('child_process');
+const ps = require('ps-node');
+const fs = require('fs');
+
+const initIniFile = (/*{ port = 33061 }*/) => {
+    const port = 33061
+    const initContent =
+        `[client]
+port=${port}
+[mysql]
+no-beep
+[mysqld]
+port=${port}
+datadir=${__dirname}\\mysql\\data\\Data
+default-storage-engine=INNODB
+sql-mode="ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION"
+log-output=FILE
+general-log=0
+general_log_file="DESKTOP-7QD7IA6.log"
+slow-query-log=1
+slow_query_log_file="DESKTOP-7QD7IA6-slow.log"
+long_query_time=10
+log-error="DESKTOP-7QD7IA6.err"
+server-id=1
+lower_case_table_names=1
+secure-file-priv="${__dirname}\\mysql\\data\\Uploads"
+max_connections=151
+table_open_cache=2000
+tmp_table_size=141M
+myisam_max_sort_file_size=2146435072
+myisam_sort_buffer_size=271M
+key_buffer_size=8M
+read_buffer_size=128K
+read_rnd_buffer_size=256K
+innodb_flush_log_at_trx_commit=1
+innodb_log_buffer_size=16M
+innodb_buffer_pool_size=128M
+innodb_log_file_size=48M
+innodb_thread_concurrency=17
+innodb_autoextend_increment=64
+innodb_buffer_pool_instances=8
+innodb_concurrency_tickets=5000
+innodb_old_blocks_time=1000
+innodb_stats_on_metadata=0
+innodb_file_per_table=1
+innodb_checksum_algorithm=0
+flush_time=0
+join_buffer_size=256K
+max_allowed_packet=4M
+max_connect_errors=100
+open_files_limit=4161
+sort_buffer_size=256K
+binlog_row_event_max_size=8K
+sync_master_info=10000
+sync_relay_log=10000
+sync_relay_log_info=10000`;
+    return new Promise(resolve => {
+        fs.writeFile(`${__dirname}/mysql/data/pmarket.ini`, initContent, (err) => {
+            console.log(err);
+            resolve({ err });
+        })
+    })
+}
+
+const killServer = () => {
+    return new Promise(resolve => {
+        si.processes().then(data => {
+            const index = data.list.findIndex(item => item.name == 'pmarket_mysqld.exe');
+            if (index < 1) {
+                throw('process not found');
+            }
+            console.log("index: ", index, "pid: ", data.list[index].pid)
+            // ps.kill(data.list[index].pid, {
+            //     signal: 'SIGKILL',
+            //     timeout: 3,  // will set up a ten seconds timeout if the killing is not successful
+            // }, function (err) {
+            //     if (err) {
+            //         console.log(err);
+            //     }
+            //     else {
+            //         console.log('Process %s has been killed!', pid);
+            //     }
+            //     resolve();
+            // });
+            child_process.exec(`taskkill /f /pid ${data.list[index].pid}`, (error, stdout, stderr) => {
+                console.log('mysql killed');
+                resolve();
+            });
+        }).catch(err => {
+            console.log(err);
+            resolve();
+        })
+    });
+}
+
+const startServer = () => {
+    return new Promise(resolve => {
+        killServer().then(() => {
+            initIniFile().then(() => {
+                let isResolved = false;
+                const process = child_process.exec(`\"${__dirname}\\mysql\\server\\bin\\pmarket_mysqld.exe\" --defaults-file=\"${__dirname}\\mysql\\data\\pmarket.ini\"`, (error, stdout, stderr) => {
+                    console.log("error:", error);
+                    console.log("stdout:", stdout);
+                    console.log("stderr:", stderr);
+                    if (!isResolved) {
+                        isResolved = true;
+                        resolve();
+                    }
+                });
+
+                process.on("spawn", () => {
+                    console.log('spawn');
+                    if (!isResolved) {
+                        isResolved = true;
+                        resolve();
+                    }
+                });
+                // mysql_process.on("close", (number, signal) => console.log("close:", number, signal));
+                // mysql_process.on("disconnect", () => console.log("disconnect"));
+                // mysql_process.on("error", (err) => console.log("error:", err));
+                // mysql_process.on("exit", (number, signal) => console.log("exit:", number, signal));
+                // mysql_process.on("message", (message) => console.log("message:", message));
+            })
+        })
+    })
+}
+
+module.exports = {
+    initIniFile,
+    startServer,
+    killServer
+};
